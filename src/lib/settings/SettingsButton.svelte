3c6c3a29f9901451d65401b5bd7125d24f515b07
Fractal <braden@fractal-hq.com>
2021-03-23 20:50:48 -0400

    Added settings button

---
+++ src/lib/settings/SettingsButton.svelte
@@ -0,0 +1,14 @@
+<div class="settings-button">⚙️</div>
+
+<style>
+	.settings-button {
+		position: absolute;
+		top: 0;
+		right: 3px;
+
+		padding: 9px;
+
+		font-size: 25px;
+		cursor: pointer;
+	}
+</style>
--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,11 +1,18 @@
 <script>
 	import Themer from '../lib/themes/Themer.svelte';
 	import { layout, url } from '@roxi/routify';
+	import SettingsButton from '../lib/settings/SettingsButton.svelte';
+
+	const r = (max = 255) => Math.floor(Math.random() * Math.floor(max));
+
+	const rgba = (opacity = 0.1) => [r(), r(), r(), opacity];
 </script>
 
-<div id="app">
+<div
+	id="app"
+	style="background-image: linear-gradient(to top, rgba({rgba()}), rgba({rgba()}))"
+>
 	<Themer size={50} />
-
 	<nav>
 		{#each $layout.children as { path, title }}
 			<a href={path} use:$url>{title}</a>
@@ -15,6 +22,8 @@
 	<main>
 		<slot />
 	</main>
+
+	<SettingsButton />
 </div>
 
 <style>
@@ -25,13 +34,13 @@
 		min-height: 100vh;
 	}
 	nav {
-		display: flex;
 		justify-content: space-around;
 		flex-direction: row-reverse;
 		padding: 1rem;
+		display: flex;
 	}
 	a {
-		text-decoration: none;
 		color: rgb(var(--dark-a));
+		text-decoration: none;
 	}
 </style>
