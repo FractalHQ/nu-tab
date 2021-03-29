3066624f698e9bd0a1ca73635461929887ea0c19
Fractal <braden@fractal-hq.com>
2021-03-28 20:24:27 -0400

    started search bar

---
+++ src/lib/ui/Search.svelte
@@ -0,0 +1,23 @@
+<center>
+	<textarea id="search" />
+</center>
+
+<style>
+	#search {
+		background: rgb(var(--light-a));
+		border: 1px solid rgba(var(--dark-b), 0.5);
+		width: 400px;
+		border-radius: 20px;
+		resize: false;
+		color: rgb(var(--dark-a));
+		line-height: 100%;
+		text-align: justify;
+		-moz-text-align-last: center; /* Firefox 12+ */
+		text-align-last: center;
+		padding: top;
+	}
+	#search:focus {
+		outline: none;
+		border: 1px solid rgba(var(--primary-a), 0.75);
+	}
+</style>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,12 +1,13 @@
 <script>
 	import Collection from '../lib/ui/Collection.svelte';
+	import Search from '../lib/ui/Search.svelte';
 </script>
 
 <!-- routify:options title="Home" -->
 <!-- routify:options index=true -->
 
 <br />
-<br />
+<Search />
 <br />
 
 <Collection />
