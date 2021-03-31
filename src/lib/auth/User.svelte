a8ba348bf04e8f2c8e02de3195ec1536dcffe749
Fractal <55504972+FractalHQ@users.noreply.github.com>
2021-03-31 00:29:29 -0400

    brick wall with blockstack/connect

---
+++ src/lib/auth/User.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { localStorageStore } from '../utils/localStorageStore';
+
+	const username = localStorageStore(
+		'username',
+		document.location.pathname.split('/')[2],
+	);
+</script>
