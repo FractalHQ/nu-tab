121d5b5101a1e37a8d1490f1f66506cc54df77ad
Fractal <55504972+FractalHQ@users.noreply.github.com>
2021-05-01 20:34:10 -0400

    cleanup

--- src/lib/settings/Settings.svelte
+++ src/lib/settings/Settings.svelte
@@ -1,17 +1,3 @@
-<!-- <script context="module">
-	import { writable } from 'svelte/store';
-
-	const randomColor = () =>
-		'#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
-	export const bookmark_settings = writable({
-		url: 'https://www.google.com',
-		color: randomColor(),
-		image: null,
-		title: '🔗',
-		description: '',
-	});
-</script> -->
-
 <script>
 	import SettingsButton from './SettingsButton.svelte';
 </script>
